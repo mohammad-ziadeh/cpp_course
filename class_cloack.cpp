0ea483eaaa6f3c3d39f5ff6053187cb798eb7786
mohammad <mohammedziadeh6@gmail.com>
2026-03-11 12:07:56 +0300

    Define Clock class with time manipulation methods

--- class_cloack.cpp
+++ class_cloack.cpp
@@ -1,1 +1,36 @@
+/******************************************************************************
 
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+class Clock {
+	// already private
+	int sec;
+	int min;
+	int hr;
+
+
+public:
+	void setTime(const int&, const int&,const int&);
+	void printTime();
+	void incHr();
+	void incMin();
+	void incSec();
+	bool compare();
+
+}
+
+
+int main()
+{
+
+	std::cout<<"Hello World";
+
+	return 0;
+}
