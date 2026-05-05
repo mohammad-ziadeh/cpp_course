071b225cd75fd70dc327577a8160289866ba3d91
mohammad <mohammedziadeh6@gmail.com>
2026-05-05 19:33:13 +0300

    Update struct review with additional details
    Added additional information and clarifications about structs in C++.

---
+++ Quick review/struct_review.cpp
@@ -0,0 +1,29 @@
+ 1. A struct is a collection of a fixed number of components
+
+ 2. Components of a struct can be of different types
+
+ 3. The syntax to define a struct is:
+struct structName
+{
+  dataTypel identifier1;
+
+  dataType2 identifier2;
+};
+
+ 4. In C++, struct is a reserved word. (u cant use it for other things)
+
+ 5. In C++, struct is a definition; no memory is allocated. Memory is allocated for the struct variables only when you declare them
+
+ 6. Components of a struct are called members of the struct
+  
+ 7. Components of a struct are accessed by name => useing (.) operator
+
+ 8. The only built-in operations on a struct are the assignment ( = ) and member access operations ( . )
+
+ 9. Neither arithmetic nor relational operations are allowed on struct (s)
+
+ 10. As a parameter to a function, a struct can be passed either by value or by reference
+
+ 11. A function can return a value of type struct
+ 
+ 12. A struct can be a member of another struct
