afee10504fcf89416b59a25c4f8ee1e83548fb48
mohammad <mohammedziadeh6@gmail.com>
2026-06-06 17:22:57 +0300

    Update and rename homework_eveything.cpp to 3-classes.cpp

--- 3-classes.cpp
+++ 3-classes.cpp
@@ -1,10 +1,5 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+// محمد عماد زياده
+// 3240615
 
 #include <iostream>
 using namespace std;
